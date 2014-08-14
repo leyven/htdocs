2e293c655f4e08223462a929d1bf1147d953e92e
leyven <nyhedgg@gmail.com>
2014-08-14 10:41:03 -0500

    todo

---
+++ estancia/app/controllers/ProductoController.php
@@ -0,0 +1,37 @@
+<?php  
+
+/**
+* 
+*/
+class ProductoController extends BaseController
+{
+	public function vistaRegistro()
+	{
+		return View::make("registro");
+	}
+	public function registrar(){
+
+		$nombre =$_POST["nombre"];
+		$correo =$_POST["Correo"];
+		$pass =$_POST["pass"];
+		Session::put('nombre',$nombre);
+		try{
+			$id =DB::table('usuario')->insertGetId(array(
+
+			'nombreEstudiante'=>$nombre,
+			'passwordEstudiante'=>$pass,
+			'correoEstudiante'=>$correo
+			
+			)
+		);
+			return Redirect::to("inicio");
+		}
+		catch(ErrorException $e)
+		{
+			return View::make("login");
+		}
+		
+		
+	}
+}
+?>
